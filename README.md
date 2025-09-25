0b6f477d31fa1bb998b2a0aca3c973793e0d996d
finnensley <finniganensley@gmail.com>
2025-09-25 15:16:09 -0600

    added useRef and useEffect

--- README.md
+++ README.md
@@ -4,3 +4,17 @@
 ## Pick an api from rapid api
 ## Create a dashboard that displays data
 ## Include: useEffect with axios/api and theme toggle, useState with a loading component, useRef, and a custom Hook. 
+
+<!-- rapidapi = https://rapidapi.com/h0p3rwe/api/youtube-search-and-download/playground/apiendpoint_0db12ce3-a5cd-471d-a7ca-25f7240dcf7b -->
+
+
+# Done:
+## api chosen and implemented with axios using a custom hook
+
+# Challenge:
+## useEffect - theme toggle
+## useState - loading component
+## useRef
+## Tailwindcss to format dashboard
+## Add localStorage to keep last search videos up
+## Create a focus on the Search button to allow enter as the click - useRef ? 
--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,58 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import "./App.css";
 import useFetchData from "./custom-hooks/useFetchData";
 
 function App() {
-  // const [loading, setLoading] = useState(false);
+  
   const [data, error, getData] = useFetchData();
   const [query, setQuery] = useState("");
+  const buttonRef = useRef(null); // use to press enter 
+  const [loading, setLoading] = useState(false);
 
   //Handler for search button
   const handleSearch = () => {
     if (query.trim()) {
+      setLoading(true); //Starts loading
       getData(query);
-      setQuery(""); // this clears the input field 
+      buttonRef.current?.focus()
     }
   };
+
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      handleSearch();
+      buttonRef.current?.focus();
+    }
+  };
+
+ 
+    useEffect(() => {
+      if (loading) {
+        const timer = setTimeout(() => 
+        setLoading(false), 2000);
+        return () => clearTimeout(timer);
+    }
+  }, [loading]);
 
   return (
     <>
       <div className="card">
-        <label htmlFor="video">Search Video</label>
+        <label htmlFor="query">Search Video</label>
         <input
           className="border border-white text-white p-1 ml-2"
           type="text"
           value={query}
           name="query"
           onChange={(e) => setQuery(e.target.value)}
+          onKeyDown={handleKeyDown}
         />
-        <button onClick={handleSearch}>Search</button>
+        <button ref={buttonRef} onClick={handleSearch}>Search</button>
         {/* map over the array of videos that are returned */}
         <div>
-          {data && data.contents && data.contents.length > 0 ? (
+          {loading ? (
+            <div>...loading</div>
+          ) : data && data.contents && data.contents.length > 0 ? (
             <ul>
               {data.contents.map((item, idx) =>
                 item.video ? (
